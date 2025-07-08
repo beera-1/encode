0d8a1290400eca926b70bd860d0bbee91d65234e
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-07-08 23:11:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ API_ID = "7041911"         # 🔁 Replace with your API ID
 API_HASH = "abab2561c71e3004a55d4ff9763d5383" # 🔁 Replace with your API HASH
 BOT_TOKEN = "7903116783:AAHby3o-3EdnkZYgf9SbCqcj1u813a7V3Bk"  # 🔁 @BotFather token
 WEBHOOK_URL = "https://striking-madel-harshuuuuuuu-ab20ef70.koyeb.app"  # 🔁 Your deployed Koyeb URL
+STRING_SESSION = "BQGQUGgAwFVTPZRnyDG-HuBXtr10RllsW-rAMMulXi2udTTdUe6IT3TAJf7WKrItoFfEWG3FOczao2KItfOc8ovHdQ5nCLPoDvPPABw551-5yFV5DJSKSU1WeIdt62GgbpDlHmA2JBIeUBQeBtpdoHiDxcDLtyv40M1IomQq4RV9gYwHrNysQ-RXS-GWqF9Dz3NeMKM0GYKX_jmOSxNr5ZP9K5bmQXxj-zEWXSbHpTnMU8_Z2ImDUIXui1sN40d0uXSMOoJb-VVl2d7ylM-ethXbLAzZ_fs0tjhXobLIWLaLuMVOWzx4uA9uKGXP4eoQK2eTILnkqf6Y5yg1lFJNdbO7VawfawAAAAHHNuKtAA"
