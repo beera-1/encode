9189bedc83c6ad370b82f07dd6c4928ac362f950
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-07-08 18:58:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # 📁 config.py
 
-API_ID = 12345678          # 🔁 Replace with your API ID
-API_HASH = "your_api_hash" # 🔁 Replace with your API HASH
-BOT_TOKEN = "your_bot_token"  # 🔁 @BotFather token
+API_ID = 7041911         # 🔁 Replace with your API ID
+API_HASH = "abab2561c71e3004a55d4ff9763d5383" # 🔁 Replace with your API HASH
+BOT_TOKEN = "7903116783:AAHvwR-lnaKlMpqKHarfI5faseLIYIxLiHU"  # 🔁 @BotFather token
 WEBHOOK_URL = "https://your-koyeb-app.koyeb.app"  # 🔁 Your deployed Koyeb URL
